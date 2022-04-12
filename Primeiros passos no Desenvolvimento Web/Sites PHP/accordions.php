e1e689d76eb0ca3d522975914b020a74c10b3bfc
William <will.vieira3107@gmail.com>
2022-04-12 08:36:03 -0300

    Adiciona exemplos/desafio em PHP realizados

---
+++ Primeiros passos no Desenvolvimento Web/Sites PHP/accordions.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Desafio Site PHP - Botões Accordion</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>O que é o PHP?</h3>
+  <div>
+    <p>
+    O PHP (um acrônimo recursivo para PHP: Hypertext Preprocessor) é uma linguagem de script 
+	open source de uso geral, muito utilizada, e especialmente adequada para o desenvolvimento web e que pode ser embutida dentro do HTML.
+    </p>
+  </div>
+  <h3>Quando foi criado o PHP?</h3>
+  <div>
+    <p>
+    Criado em 1994 por Rasmus Lerdof, a primeira encarnação do PHP foi um simples conjunto de binários Common Gateway Interface (CGI) 
+	escrito em linguagem de programação C. Em Junho de 1995, Rasmus liberou o código fonte do PHP para o público, 
+	o que permitiu que desenvolvedores o utilizassem da forma como desejassem.
+    </p>
+  </div>
+  <h3>Principais site/apps que usam PHP</h3>
+  <div>
+    <p>
+    Algumas das grandes empresas que usam o PHP: Facebook, Google, Baidu, Wikipedia, Spotify, Uber etc.
+    </p>
+    <ul>
+      <li>Facebook</li>
+      <li>Google</li>
+      <li>Baidu</li>
+	  <li>Wikipedia</li>
+	  <li>Spotify</li>
+	  <li>Uber</li>
+    </ul>
+  </div>
+  <h3>Por que utilizar o PHP?</h3>
+  <div>
+    <p>
+    O usuário pode usar PHP para programar em qualquer projeto e, depois, comercializá-lo tranquilamente. 
+	Além disso, a outra vantagem é a possibilidade de melhorias constantes no código, algo que a comunidade de programadores costuma fazer.
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
